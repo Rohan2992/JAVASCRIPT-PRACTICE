6b12290b41840f25a554a6f4218a1996d26666de
Rohan2992 <rohan.chaudhary29.rc@gmail.com>
2023-08-21 11:05:29 +0530

    Added delete route

--- Week 3 To-Do/server/index.js
+++ Week 3 To-Do/server/index.js
@@ -9,9 +9,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "..", "/client")));
 
-let toDoList = [];
-
 // todo: get using array
+let toDoList = [];
 
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "../client/index.html"));
@@ -62,9 +61,15 @@ function findIndex(arr, id) {
 function removeIndex(arr, id) {
   return arr.filter(x => x.id !== id);
 }
- 
+
 app.delete("/todos/:id", (req, res) => {
   const id = parseInt(req.params.id);
+
+  fs.readFile("./files.json", "utf-8", (err, data) => {
+    if (!err) {
+      toDoList = JSON.parse(data);
+    }
+  });
 
   const index = findIndex(toDoList, id);
 
@@ -72,6 +77,9 @@ app.delete("/todos/:id", (req, res) => {
     res.status(404).send("No Such item exists in record.");
   } else {
     toDoList = removeIndex(toDoList, id);
+    fs.writeFile("./files.json", JSON.stringify(toDoList), err =>
+      console.log(err)
+    );
     res.status(200).send(toDoList);
   }
 });
