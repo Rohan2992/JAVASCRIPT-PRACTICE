ad9cf1d5b5f68589ee795c3e027d17c24cd41d56
Rohan2992 <rohan.chaudhary29.rc@gmail.com>
2023-08-26 03:02:42 +0530

    Delete function and styling the list button

--- Week 3 To-Do/client/script.js
+++ Week 3 To-Do/client/script.js
@@ -8,7 +8,10 @@ async function showList() {
 
   let li = "";
   for (var i = 0; i < Data.length; i++) {
-    li += `<li>${Data[i].id} - ${Data[i].title} - ${Data[i].description}</li>`;
+    li += `<li class="listItem" value= ${Data[i].id}} > ${Data[i]
+      .title} - ${Data[i]
+      .description}  <button onClick="deleteElementFromList(${Data[i]
+      .id})" class="deleteBtn button" type="submit"><span>Delete</span></button> </li>`;
   }
 
   const ul = document.querySelector(".toDoList");
@@ -28,5 +31,20 @@ async function postData() {
     }),
     headers: { "Content-Type": "application/json" }
   });
+  title.value = "";
+  description.value = "";
   showList();
+}
+
+async function deleteElementFromList(id) {
+  const Fetch = await fetch("http://localhost:3000/todos/" + id, {
+    method: "DELETE",
+    headers: { "Content-Type": "application/json" }
+  });
+  await Fetch.json();
+  // console.log(data);
+  showList();
+  // console.log(data);
+  // console.log(listElement.textContent.substring(0, 5));
+  // console.log("I am clicked");
 }
--- Week 3 To-Do/server/index.js
+++ Week 3 To-Do/server/index.js
@@ -12,45 +12,6 @@ app.use(express.static(path.join(__dirname, "..", "/client")));
 // todo: get using array
 let toDoList = [];
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/index.html"));
-});
-
-app.get("/todos", (req, res) => {
-  // using the variable todo list
-  // res.send(toDoList);
-
-  // using the json file
-  fs.readFile("./files.json", "utf-8", function(err, data) {
-    if (!err) console.log(data);
-    const json = JSON.parse(data);
-    res.send(json);
-  });
-});
-
-app.post("/todos", (req, res) => {
-  // console.log(req.body);
-
-  const toDoObj = {
-    id: Math.floor(Math.random() * 100000),
-    title: req.body.title,
-    description: req.body.description
-  };
-
-  // using the variable todo list
-  // toDoList.push(toDoObj);
-  fs.readFile("./files.json", "utf-8", (err, data) => {
-    if (!err) {
-      let Data = JSON.parse(data);
-      Data.push(toDoObj);
-      fs.writeFile("./files.json", JSON.stringify(Data), err => {
-        if (err) throw err;
-        res.json(toDoObj);
-      });
-    }
-  });
-});
-
 function findIndex(arr, id) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i].id === id) return i;
@@ -61,6 +22,47 @@ function findIndex(arr, id) {
 function removeIndex(arr, id) {
   return arr.filter(x => x.id !== id);
 }
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/index.html"));
+});
+
+app.get("/todos", (req, res) => {
+  // using the variable todo list
+  // res.send(toDoList);
+
+  // using the json file
+  fs.readFile("./files.json", "utf-8", function(err, data) {
+    if (err) console.log(err);
+    const json = JSON.parse(data);
+    res.send(json);
+  });
+});
+
+app.post("/todos", (req, res) => {
+  if (req.body.title == "" || req.body.description == "") {
+    res.send("Bad Request sent fields cannot be empty");
+  } else {
+    const toDoObj = {
+      id: Math.floor(Math.random() * 100000),
+      title: req.body.title,
+      description: req.body.description
+    };
+
+    // using the variable todo list
+    // toDoList.push(toDoObj);
+    fs.readFile("./files.json", "utf-8", (err, data) => {
+      if (!err) {
+        let Data = JSON.parse(data);
+        Data.push(toDoObj);
+        fs.writeFile("./files.json", JSON.stringify(Data), err => {
+          if (err) throw err;
+          res.json(toDoObj);
+        });
+      }
+    });
+  }
+});
 
 app.delete("/todos/:id", (req, res) => {
   const id = parseInt(req.params.id);
@@ -73,14 +75,14 @@ app.delete("/todos/:id", (req, res) => {
 
   const index = findIndex(toDoList, id);
 
-  if (index === -1) {
-    res.status(404).send("No Such item exists in record.");
-  } else {
+  if (index !== -1) {
     toDoList = removeIndex(toDoList, id);
     fs.writeFile("./files.json", JSON.stringify(toDoList), err =>
       console.log(err)
     );
     res.status(200).send(toDoList);
+  } else {
+    res.status(404).json("No Such item exists in record.");
   }
 });
 
