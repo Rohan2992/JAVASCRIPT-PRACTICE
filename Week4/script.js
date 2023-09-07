98b33b7c7db05dec137a9666c410118dfa165286
Rohan2992 <rohan.chaudhary29.rc@gmail.com>
2023-09-07 18:59:44 +0530

    reconciler completed

--- Week4/script.js
+++ Week4/script.js
@@ -1,38 +1,36 @@
-function domManipulate(toDoList) {
+let vDOM = [];
+
+function domManipulate(existingDOM) {
   let added = 0;
   let updated = 0;
   let deleted = 0;
 
   const parent = document.getElementById("main-Area");
 
-  let existingChildren = Array.from(parent.children);
   //   console.log(arr);
 
-  toDoList.forEach(toDo => {
-    const elementExists = existingChildren.find(ec => {
-      return ec.dataset.id === String(toDo.id);
+  vDOM.forEach(item => {
+    const elementExists = existingDOM.find(ec => {
+      return ec.id === item.id;
     });
     // console.log(elementExists);
 
     if (elementExists) {
       updated++;
-      elementExists.children[0].innerHTML = toDo.title;
-      elementExists.children[1].innerHTML = toDo.description;
-
-      existingChildren = existingChildren.filter(ec => {
-        return ec !== elementExists;
-      });
+      var existingElement = document.querySelector(`[data-id="${item.id}"]`);
+      existingElement.children[0].innerHTML = item.title;
+      existingElement.children[1].innerHTML = item.description;
     } else {
       added++;
       // console.log(element);
       const children = document.createElement("div");
-      children.dataset.id = toDo.id;
+      children.dataset.id = item.id;
       const span1 = document.createElement("span");
-      span1.innerHTML = toDo.title;
+      span1.innerHTML = item.title;
       const span2 = document.createElement("span");
-      span2.innerHTML = toDo.description;
+      span2.innerHTML = item.description;
       const button = document.createElement("button");
-      button.innerHTML = toDo.id;
+      button.innerHTML = item.id;
 
       children.appendChild(span1);
       children.appendChild(span2);
@@ -41,11 +39,29 @@ function domManipulate(toDoList) {
     }
   });
   //   console.log(existingChildren);
-  existingChildren.forEach(ec => {
-    deleted++;
-    parent.removeChild(ec);
+
+  existingDOM.forEach(item => {
+    if (!vDOM.some(a => a.id === item.id)) {
+      deleted++;
+      var childToRemove = document.querySelector(`[data-id="${item.id}"]`);
+      parent.removeChild(childToRemove);
+    }
   });
   console.log(added, updated, deleted);
+}
+
+function updateVirtualDOM(toDoList) {
+  let existingDOM = [...vDOM];
+
+  vDOM = toDoList.map(e => {
+    return {
+      title: e.title,
+      description: e.description,
+      id: e.id
+    };
+  });
+
+  domManipulate(existingDOM);
 }
 
 const interval = setInterval(() => {
@@ -57,7 +73,7 @@ const interval = setInterval(() => {
       id: i + 1
     });
   }
-  domManipulate(toDoList);
+  updateVirtualDOM(toDoList);
 }, 1000);
 
 const button = document.getElementById("button");
