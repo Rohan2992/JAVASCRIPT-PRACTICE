ef0726d938419c78ce41946e6e01358e8ca18f4d
Rohan2992 <rohan.chaudhary29.rc@gmail.com>
2023-09-10 03:03:09 +0530

    Hools completed successfully

--- Week4/React-starting/src/App.jsx
+++ Week4/React-starting/src/App.jsx
@@ -1,32 +1,45 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 
-function App() {
+function useToDo() {
   const [toDo, setToDo] = useState([
     { title: "Gym", description: "Go To Gym" },
     { title: "School", description: "Go To School" }
   ]);
-  useEffect(async () => {
-    console.log("Inside useEffect");
+
+  useEffect(() => {
+    async function fetchData() {
+      const Fetch = await fetch("http://localhost:3000/todos");
+      const data = await Fetch.json();
+
+      setToDo(data);
+    }
+
+    const cls = setInterval(() => {
+      fetchData();
+    }, 1000);
+
+    return () => clearInterval(cls);
   }, []);
-  console.log("render");
+  return toDo;
+}
+
+function App() {
+  const toDo = useToDo();
 
   return (
     <div>
-    {toDo.map((e)=>{
-      return (
-        <>
-        <h1>
-        {e.title}
-      </h1>
-      <p>
-        {e.description}
-      </p>
-      <br />
-      </>
-) 
-    })}
-            
+      {toDo.map(e =>
+        <div key={e.key}>
+          <h1>
+            {e.title}
+          </h1>
+          <p>
+            {e.description}
+          </p>
+          <br />
+        </div>
+      )}
     </div>
   );
 }
